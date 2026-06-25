897aafd950215251e42644014cc4cabbacc78931
David Davidson <ddavidson644@gmail.com>
2026-06-25 14:42:37 -0400

    Implementing logic for matching buy orders with best ask orders

--- src/main/java/MatchingEngine.java
+++ src/main/java/MatchingEngine.java
@@ -1,5 +1,6 @@
 import java.util.Comparator;
 import java.util.Deque;
+import java.util.Map;
 import java.util.TreeMap;
 
 public class MatchingEngine {
@@ -7,4 +8,13 @@ public class MatchingEngine {
     private final TreeMap<Long, Deque<Order>> bids = new TreeMap<>(Comparator.reverseOrder());
 
     private final TreeMap<Long, Deque<Order>> asks = new TreeMap<>();
+
+    private void matchBuy(Order buyOrder) {
+        while (buyOrder.getQuantity() > 0 && !asks.isEmpty()) {
+            Map.Entry<Long, Deque<Order>> bestAskEntry  = asks.firstEntry();
+            long askPrice = bestAskEntry.getKey();
+
+            if (askPrice > buyOrder.getPrice()) break;
+        }
+    }
 }
